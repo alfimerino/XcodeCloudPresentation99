3058c04865c7656298ac97685b9236d5e14b5fe2
Alfredo Merino <alfimerino@gmail.com>
2024-02-29 11:16:28 -0500

    Initial Commit

---
+++ XcodeCloudPresentation99/XcodeCloudPresentation99App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudPresentation99App.swift
+//  XcodeCloudPresentation99
+//
+//  Created by Alfredo Merino on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudPresentation99App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
